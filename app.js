86332e196e87fb733d8f09cf36c1ff57cfd42a61
Miguel Bugalho <miguel.bugalho@gmail.com>
2024-05-16 09:44:59 +0100

    Added the code for a generic static web server - package.json with express and mysql dependencies - settings files - app.js with a simple express server with a "www" static files folder

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var settings = require("./config/settings.json").server;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'www')));
+
+// when we don't find anything
+app.use((req, res, next) => {
+    res.status(404).send({ msg: "No resource or page found." });
+})
+
+// When we find an error (means it was not treated previously)
+app.use((err, req, res, next) => {
+    console.error(err)
+    res.status(500).send(err);
+})
+
+const port = parseInt(process.env.port || settings.port);
+app.listen(port, function () {
+    console.log("Server running at http://localhost:" + port);
+});
